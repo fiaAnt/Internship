e0b6f54a9e1672754060a058a690e79120647ed4
fiaAnt <fiablack842@gmail.com>
2025-12-23 16:24:15 +0300

    refactor: use env vars and validate product price in tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,13 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   video: true,
   videosFolder: 'cypress/videos',
+  projectId: null,
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     setupNodeEvents(on, config) {},
+  },
+  env: {
+    USERNAME: 'standard_user',
+    PASSWORD: 'secret_sauce',
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,11 @@
 describe('test case', () => {
+  const username = Cypress.env('USERNAME');
+  const password = Cypress.env('PASSWORD');
+
   it('Finds the cheapest item and verifies it in cart', () => {
     cy.visit('/');
-    cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('secret_sauce');
+    cy.get('#user-name').type(username);
+    cy.get('#password').type(password);
     cy.get('#login-button').click();
 
     cy.get('[data-test="product-sort-container"]').as('sort').should('exist');
@@ -23,32 +26,47 @@ describe('test case', () => {
         cy.contains('Add to cart').click();
       });
 
-    cy.get('@productName').then((name) => {
-      cy.get('.shopping_cart_link').click();
-      cy.url().should('include', '/cart.html');
+    cy.get('@productName').then((productName) => {
+      cy.get('@productPrice').then((productPrice) => {
+        cy.get('.shopping_cart_link').click();
+        cy.url().should('include', '/cart.html');
+        cy.get('.inventory_item_name')
+          .first()
+          .should(
+            'contain',
+            productName.trim(),
+            'ERROR: item name in cart does not match selected'
+          );
+        cy.get('.inventory_item_price')
+          .first()
+          .should(
+            'contain',
+            productPrice.trim(),
+            'ERROR: item price in cart does not match selected'
+          );
 
-      cy.get('.inventory_item_name')
-        .first()
-        .should(
-          'contain',
-          name,
-          'ERROR: item name in cart does not match selected'
-        );
+        cy.get('#checkout').click();
+        cy.get('#first-name').type('Test');
+        cy.get('#last-name').type('User');
+        cy.get('#postal-code').type('10101');
+        cy.get('#continue').click();
 
-      cy.get('#checkout').click();
-      cy.get('#first-name').type('Test');
-      cy.get('#last-name').type('User');
-      cy.get('#postal-code').type('10101');
-      cy.get('#continue').click();
-
-      cy.url().should('include', '/checkout-step-two.html');
-      cy.get('.cart_item .inventory_item_name')
-        .first()
-        .should(
-          'contain',
-          name,
-          'ERROR: Checkout item does not match selected'
-        );
+        cy.url().should('include', '/checkout-step-two.html');
+        cy.get('.cart_item .inventory_item_name')
+          .first()
+          .should(
+            'contain',
+            productName.trim(),
+            'ERROR: Checkout item does not match selected'
+          );
+        cy.get('.cart_item .inventory_item_price')
+          .first()
+          .should(
+            'contain',
+            productPrice.trim(),
+            'ERROR: Checkout price does not match selected'
+          );
+      });
     });
     cy.get('#finish').click();
     cy.get('.complete-header').should(
