52b21f53e899c643631b3b2b101761a53d6b65fb
fiaAnt <fiablack842@gmail.com>
2026-01-06 14:27:04 +0300

    chore(cypress): remove unused projectId and setupNodeEvents

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   video: true,
   videosFolder: 'cypress/videos',
-  projectId: null,
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
-    setupNodeEvents(on, config) {},
   },
   env: {
     USERNAME: 'standard_user',
